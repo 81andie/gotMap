cf9c6d53fcff7554b69945db53820d9cb6cb79ff
81andie <133395577+81andie@users.noreply.github.com>
2026-03-03 22:06:32 +0100

    Creando los componentes, servicios i interfaces

---
+++ src/app/components/mini-buscador/mini-buscador.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-buscador',
+  imports: [],
+  templateUrl: './mini-buscador.html',
+  styleUrl: './mini-buscador.css',
+})
+export class MiniBuscador {}
